dffaf72c1954dc1247ff926dabfe8a4e9cf795ad
nguignard <nguignard@gmail.com>
2017-09-25 16:54:11 +0200

    p11

--- MetierServicesApp/IServiceStagiaires.cs
+++ MetierServicesApp/IServiceStagiaires.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using Exo9;
 
 //using classesMetier;
 
@@ -17,6 +18,37 @@ namespace MetierServicesApp
         [OperationContract]
         string GetData(int value);
 
+        [OperationContract]
+        MSection GetSection(string codeCollection);
+
+        [OperationContract]
+        IList<MStagiaire> GetStagiairesSection(string codeSection);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns>Retourne une string vide en cas de succès ou une string contenant le message d'erreur en cas d'échec</returns>
+        [OperationContract]
+                string AddStagiaire(MStagiaire newStagiaire, MSection laSection);
+
+
+        /// <summary>
+        /// Update un stagiaire
+        /// </summary>
+        /// <param name="leStagiaire"></param>
+        /// <returns>Retourne une string vide en cas de succès ou une string contenant le message         d'erreur en cas d'échec</returns>
+        [OperationContract]
+        string UpdateStagiaire(MStagiaire leStagiaire);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="laClefStagiaire"></param>
+        /// <returns>return true si le stagiaire est bien supprimé</returns>
+        [OperationContract]
+        bool DeleteStagiaire(Int32 laClefStagiaire);
 
 
 
